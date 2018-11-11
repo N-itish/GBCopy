05444b668817a3e7c5c40a93a54ca8a82d795a99
N-itish <manandharnara@gmail.com>
2018-11-11 13:02:20 +0545

    added filter

--- FileCopier/src/com/nitish/main/Main.java
+++ FileCopier/src/com/nitish/main/Main.java
@@ -11,12 +11,15 @@ import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.ListIterator;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.print.attribute.standard.Destination;
 
 public class Main {
 	static String source = "D:\\";
 	static String destination = "C:\\Users\\Lenovo\\Desktop\\test.txt";
+	static String seperateFilePattern = "['.'].*";
 	static ArrayList<String> InitialFileName = new ArrayList<>();
 	public static void main(String ...args)
 	{
@@ -27,7 +30,7 @@ public class Main {
 		File Sourcefile = new File(source);
 		//selecting the destination file
 		File DestinationFile = new File(destination);
-		
+		//System.out.println(MatchFiles("D://asd"));
 		getAllFileLocation(Sourcefile);
 		Iterator<String> itr = InitialFileName.iterator();
 		try {
@@ -45,16 +48,18 @@ public class Main {
 
 		
 	}
+	//function for recursion
+	//getting all the files in the directory, sub-directory and so on
 	public static void getAllFileLocation(File Sourcefile) {
 		for(File l : Sourcefile.listFiles())
 		{
-			if(l.getPath().contains("."))
+			if(!MatchFiles(l.getPath()))
 			{
+				System.out.println(l.getPath());
 				InitialFileName.add(l.getPath());
 			}
 			try {
 				getAllFileLocation(l);
-				System.out.println("added: " + l.getPath());
 			}catch(NullPointerException NulptrExc)
 			{
 				continue;
@@ -78,4 +83,16 @@ public class Main {
 		}
 	
 	}	
+	//function to match the pattern in the file location
+	//Separating the folders and getting only the location of files with .pdf or .exe
+	public static boolean MatchFiles(String StringToCheck) 
+	{
+		boolean isMatched = true;
+		Pattern pattern = Pattern.compile(seperateFilePattern);
+		Matcher matcher = pattern.matcher(StringToCheck);
+		if(matcher.find()) {
+			isMatched = false;
+		}
+		return isMatched;	
+	}
 }
